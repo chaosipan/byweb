0e120f1bb07e9b007e207b3914fd719d82238820
dalex <lizhekang@163.com>
2015-10-06 00:13:11 +0800

    Signed-off-by: dalex <lizhekang@163.com>

--- git.js
+++ git.js
@@ -10,14 +10,14 @@ function pull_callback(code, output){
     logHelper.logH('Exit code:'+ code);
     logHelper.logH('git pull output:\n' + output);
 
-    return Thenjs(function() {});
+    //return Thenjs(function() {});
 }
 
 function pull(pwd, tasksArray) {
     var ta = tasksArray ? tasksArray : new Array();
     console.log(tasksArray);
     cd(pwd);
-    exec('git pull', {silent:true}, pull_callback.then(function() {}));
+    exec('git pull', {silent:true}, pull_callback);
 }
 
 module.exports = {
